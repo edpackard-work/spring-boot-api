1b163fe79a6be99e0b5f781d1135fe124004ada8
Ed Packard <edward.packard@kinandcarta.com>
2022-03-15 12:28:11 +0000

    Add counter tests

--- src/test/java/com/example/restservice/CounterTest.java
+++ src/test/java/com/example/restservice/CounterTest.java
@@ -1,2 +1,23 @@
-package com.example.restservice;public class CounterTest {
+package com.example.restservice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CounterTest {
+
+    @Test
+    public void testInitialisedDefaultValue() {
+        Counter counter = new Counter();
+        assertEquals(0, counter.getCount());
+    }
+
+    @Test
+    public void testIncrementation() {
+        Counter counter = new Counter();
+        counter.incrementCount();
+        assertEquals( 1, counter.getCount());
+        counter.incrementCount();
+        assertEquals( 2, counter.getCount());
+    }
 }
